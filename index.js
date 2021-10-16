cbc7aa98852ea9f6313820df410538ac4ad6daad
cyril cabrolier <cyr.cab@hotmail.fr>
2021-10-16 16:31:00 +0200

    premières lignes css

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const deathPerson = document.querySelector(".death-person");
 
 form.onsubmit = function(event) {
     event.preventDefault();
-    const dead = document.createElement("li");
-    dead.innerHTML = submitFirstname.value;
-    dead.classList.add("dead");
-    deathPerson.appendChild(dead);
 
 
+    if (submitFirstname.value === 'anas' || submitFirstname.value === 'cyril'){
+        alert("Impossible de tuer cette personne !")
+    } else {
+        const dead = document.createElement("li");
+        dead.innerHTML = submitFirstname.value;
+        dead.classList.add("dead");
+        deathPerson.appendChild(dead);
+    }
+
+
+    
 }
